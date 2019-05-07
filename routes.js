da312d1113b31c46759428ae56590885ed0ee3dc
jacecotton <jacecotton@Jaces-MacBook-Pro.local>
2019-05-07 03:16:11 -0500

    splitting portfolio controller to controllers.js from routes.js

--- routes.js
+++ routes.js
@@ -9,16 +9,7 @@ module.exports = (app) => {
   app.post("/contact", controllers.contact.post);
 
   portfolio.forEach((item) => {
-    let { name, id } = item;
-
-    app.get(`/portfolio/${id}`, (req, res) => {
-      res.render("portfolio", {...{
-        page: "portfolio", // tells the menu that the "Portfolio" link is active, so all portfolio items will keep the active class on that link
-        title: `${name} | Jace Cotton`,
-        partial: `portfolio/_${id}.njk`,
-        localcss: fs.readFileSync("./public/css/portfolio.css"),
-      },...item}); // merging the data from each object in the `portfolio` array into the object passed through `res.render`, thereby exposing its properties to the njk file.
-    });
+    app.get(`/portfolio/${item.id}`, controllers.portfolio(item));
   });
 
   app.get("/privacy", controllers.privacy);
