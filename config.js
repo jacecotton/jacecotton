3275c448f64492b6374484b8f55d85ee90306ddb
jacecotton <jacecotton@Jaces-MacBook-Pro.local>
2019-05-13 22:52:23 -0500

    cleanup

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ module.exports = {
 
   globals: {
     css: fs.readFileSync("./public/css/main.css"),
-    js: (fs.readFileSync("./public/js/non-critical-css.js") + (fs.readFileSync("./public/js/has-js.js")))
+    js: (fs.readFileSync("./public/js/non-critical-css.js") + fs.readFileSync("./public/js/has-js.js"))
   }
 };
