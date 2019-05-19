f89ce89dde91c800a9dd83c02f87affb3293a617
jacecotton <jacecotton@Jaces-MacBook-Pro.local>
2019-05-19 15:45:56 -0500

    temporarily disabling contact form due to spam

--- controllers.js
+++ controllers.js
@@ -29,41 +29,41 @@ module.exports = {
       })
     },
 
-    post: (req, res) => {
-      const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-          user: "jacedc98@gmail.com",
-          pass: process.env.GMAIL_PASS
-        },
-      });
-
-      let { name, email, subject, message } = req.body;
-
-      const mailOptions = {
-        from: "jacedc98@gmail.com",
-        to: "jacedc98@gmail.com, fitzroy3k@gmail.com",
-        subject: subject,
-        html: `
-          <strong>From:</strong> ${name} <br>
-          <strong>Sender Email:</strong> ${email} <br>
-          <strong>Message:</strong> <br>
-          ${message}
-        `,
-      };
-
-      transporter.sendMail(mailOptions, (error, info) => {
-        if(error) {
-          console.log(error);
-        } else {
-          console.log(`Email sent: ${info.response}`);
-        }
-      });
-
-      res.render("success", {
-        title: "Message Sent | Jace Cotton",
-      });
-    },
+    // post: (req, res) => {
+    //   const transporter = nodemailer.createTransport({
+    //     service: "gmail",
+    //     auth: {
+    //       user: "jacedc98@gmail.com",
+    //       pass: process.env.GMAIL_PASS
+    //     },
+    //   });
+    //
+    //   let { name, email, subject, message } = req.body;
+    //
+    //   const mailOptions = {
+    //     from: "jacedc98@gmail.com",
+    //     to: "jacedc98@gmail.com",
+    //     subject: subject,
+    //     html: `
+    //       <strong>From:</strong> ${name} <br>
+    //       <strong>Sender Email:</strong> ${email} <br>
+    //       <strong>Message:</strong> <br>
+    //       ${message}
+    //     `,
+    //   };
+    //
+    //   transporter.sendMail(mailOptions, (error, info) => {
+    //     if(error) {
+    //       console.log(error);
+    //     } else {
+    //       console.log(`Email sent: ${info.response}`);
+    //     }
+    //   });
+    //
+    //   res.render("success", {
+    //     title: "Message Sent | Jace Cotton",
+    //   });
+    // },
   },
 
   portfolio: (req, res) => {
--- routes.js
+++ routes.js
@@ -4,7 +4,7 @@ module.exports = (app) => {
   app.get("/", controllers.index);
   app.get("/about", controllers.about);
   app.get("/contact", controllers.contact.get);
-  app.post("/contact", controllers.contact.post);
+  // app.post("/contact", controllers.contact.post);
   app.get("/portfolio/:itemName", controllers.portfolio);
   app.get("/privacy", controllers.privacy);
   app.get("/terms", controllers.terms);
