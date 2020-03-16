54c7dd54eae2484282c8d974c389888ed42c1d07
Jace Cotton <jacecotton@Jaces-MacBook-Pro.local>
2020-03-16 18:48:04 -0500

    under construction

--- portfolio.js
+++ portfolio.js
@@ -1,49 +1,49 @@
 module.exports = [
-  {
-    id: "jwi",
-    name: "Jackson Warren Interiors",
-    description: "A Houston interior design firm.",
-    thumbnail: "/images/portfolio/jwi/thumb.jpg",
-    live: true,
-    site: "https://www.jacksonwarreninteriors.com/"
-  },
+  // {
+  //   id: "jwi",
+  //   name: "Jackson Warren Interiors",
+  //   description: "A Houston interior design firm.",
+  //   thumbnail: "/images/portfolio/jwi/thumb.jpg",
+  //   live: true,
+  //   site: "https://www.jacksonwarreninteriors.com/"
+  // },
 
-  {
-    id: "artisan",
-    name: "Artisan",
-    description: "A landing page design with a handcrafted æsthetic.",
-    thumbnail: "/images/portfolio/artisan/thumb.jpg",
-    live: true,
-    github: "https://github.com/jacecotton/artisan"
-  },
+  // {
+  //   id: "artisan",
+  //   name: "Artisan",
+  //   description: "A landing page design with a handcrafted æsthetic.",
+  //   thumbnail: "/images/portfolio/artisan/thumb.jpg",
+  //   live: true,
+  //   github: "https://github.com/jacecotton/artisan"
+  // },
 
-  {
-    id: "seadusk",
-    name: "Seadusk",
-    description: "An individual portfolio website theme.",
-    thumbnail: "/images/portfolio/seadusk/thumb.jpg"
-  },
+  // {
+  //   id: "seadusk",
+  //   name: "Seadusk",
+  //   description: "An individual portfolio website theme.",
+  //   thumbnail: "/images/portfolio/seadusk/thumb.jpg"
+  // },
 
-  {
-    id: "brick-mortar",
-    name: "Brick & Mortar",
-    description: "A three-page spec website theme.",
-    thumbnail: "/images/portfolio/brick-mortar/thumb.jpg"
-  },
+  // {
+  //   id: "brick-mortar",
+  //   name: "Brick & Mortar",
+  //   description: "A three-page spec website theme.",
+  //   thumbnail: "/images/portfolio/brick-mortar/thumb.jpg"
+  // },
 
-  {
-    id: "cycler",
-    name: "Cycler",
-    description: "A proof-of-concept custom shop system.",
-    thumbnail: "/images/portfolio/cycler/thumb.jpg"
-  },
+  // {
+  //   id: "cycler",
+  //   name: "Cycler",
+  //   description: "A proof-of-concept custom shop system.",
+  //   thumbnail: "/images/portfolio/cycler/thumb.jpg"
+  // },
 
-  {
-    id: "barista",
-    name: "Barista",
-    description: "An individual portfolio website theme.",
-    thumbnail: "/images/portfolio/barista/thumb.jpg"
-  },
+  // {
+  //   id: "barista",
+  //   name: "Barista",
+  //   description: "An individual portfolio website theme.",
+  //   thumbnail: "/images/portfolio/barista/thumb.jpg"
+  // },
 
   // {
   //   id: "sublime",
--- routes.js
+++ routes.js
@@ -5,7 +5,7 @@ module.exports = (app) => {
   app.get("/about", controllers.about);
   app.get("/contact", controllers.contact.get);
   // app.post("/contact", controllers.contact.post);
-  app.get("/portfolio/:itemName", controllers.portfolio);
+  // app.get("/portfolio/:itemName", controllers.portfolio);
   app.get("/privacy", controllers.privacy);
   app.get("/terms", controllers.terms);
 };
